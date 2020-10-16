390eb03e6a89290eb3512501b86eb45ed4c3f310
ouyangxiao <oyangxiao@outlook.com>
2020-10-16 13:41:26 +0800

    add vue3 remove blazor

---
+++ pages/vue3-vite/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>count is {{count}}</div>
+  <div>obj is {{obj}}</div>
+</template>
+<script lang="ts">
+import { defineComponent, ref, reactive } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const count = ref(0);
+    const obj = reactive({ count });
+    return { count, obj };
+  },
+});
+</script>
